af49e4ece09db479725c839b5ef6b8fc9c3aed28
coathangers <48773923+coathangers@users.noreply.github.com>
2020-11-16 09:13:59 +0000

    Update load huge json to postgres.py

--- load huge json to postgres.py
+++ load huge json to postgres.py
@@ -9,16 +9,16 @@ with open(sys.argv[1]) as my_file: #(SEE LINE 29)
     data = json.load(my_file)
 
 print("file loaded")
-with psycopg2.connect("dbname=XXXX user=YYYY password=ZZZZ") as conn: #REQUIRES POSTGRES DATABASE CREDENTIALS
+with psycopg2.connect("dbname=XXXX user=YYYY password=ZZZZ") as conn: #INSERT POSTGRES DATABASE CREDENTIALS + TABLE NAME ON 14
     with conn.cursor() as cur:
         cur.execute(""" drop table TABLENAME; create table TABLENAME(
-            COLUMN1 type, COLUMN2 type, COLUMN3 type) """) #REQUIRES SQL TABLE NAMES AND DATA TYPE TO MATCH JSON FILE (SEE POSTGRES DATABASE LOAD)
+            COLUMN1 type, COLUMN2 type, COLUMN3 type) """) #INSERT SQL TABLE COLUMN NAMES AND DATA TYPE TO MATCH JSON FILE (SEE EXAMPLE POSTGRES SQL)
 print("finished creating tables")
 chunks = [data[x:x+10000] for x in range(0, len(data), 10000)]
 for item in chunks:
-    with psycopg2.connect("dbname=XXXX user=YYYY password=ZZZZ") as conn: #REQUIRES POSTGRES DATABASE CREDENTIALS
+    with psycopg2.connect("dbname=XXXX user=YYYY password=ZZZZ") as conn: #INSERT POSTGRES DATABASE CREDENTIALS + TABLE NAME ON 21,22
         with conn.cursor() as cur:
-            query_sql = """ insert into TABLENAME #REQUIRES TABLE NAME
+            query_sql = """ insert into TABLENAME 
             select * from json_populate_recordset(NULL::TABLENAME, %s) """
             cur.execute(query_sql, (json.dumps(item),))
             rows += 1
@@ -27,3 +27,4 @@ for item in chunks:
 
 #print(json_data[10:])
 #execute with: python NAME.py "D:\.....json" #(ADD THE PATH TO YOUR JSON FILE)
+#requirements on lines: 2,8,12,14,15,19,21,22,29
